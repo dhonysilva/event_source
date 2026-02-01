47f590a0747dcc5880411c436f6e341fef1e494d
Dhony Silva <dhony.dev@gmail.com>
2026-01-31 22:29:26 -0300

    Add process manager module and notes on event flows
    Introduce Batch.ProcessManager for handling batch events and commands.
    Add 04_notes.md with concepts and laws for event-sourced systems.
    Comment out child processes in application supervision tree.

---
+++ 04_notes.md
@@ -0,0 +1,28 @@
+Some works to keep in mind:
+
+- commands
+- events
+- aggregates
+- projectors
+
+Emits the summary data
+
+- notifiers
+- injections
+
+## Process Managers
+
+Is responsible for managing stateful, long-running processes. Requires multiple events and commands to complete.
+
+A process manager is the inverse of an aggregate: it consumes events and emits commands.
+
+Process managers are coreographers who help event-sourced systems dance.
+
+
+### Laws
+
+- All events are immutable and exist in the past
+- All data required for a Projection must be on the Events
+- Work is a side effect
+- All Projectors must stem from Events
+- Never manage more than one flow per Process Manager
---
+++ lib/batch/process_manager.ex
@@ -0,0 +1,75 @@
+defmodule Batch.ProcessManager do
+  use GenServer
+
+  def start_link(%{id: _id} = state) do
+    GenServer.start_link(__MODULE__, state)
+  end
+
+  def init(%{id: id}) do
+    {:ok, %{id: id, files: %{}, status: :idle}}
+  end
+
+  def handle_call({:process_event, evt}, _from, state) do
+    handle_event(state, evt)
+  end
+
+  defp handle_event(
+         state,
+         %{
+           event_type: :batch_created,
+           files: files
+         }
+       ) do
+    f = Enum.map(files, fn f -> {f, :pending} end) |> Map.new()
+
+    state = %{
+      state
+      | files: f,
+        status: :created
+    }
+
+    reply =
+      Enum.map(files, fn f ->
+        %{
+          command_type: :process_file,
+          file: f
+        }
+      end)
+
+    {:reply, reply, state}
+  end
+
+  defp handle_event(state, %{
+         event_type: :file_processed,
+         file: %{id: file_id, status: file_status}
+       }) do
+    files = Map.put(state.files, file_id, file_status)
+
+    state = %{
+      state
+      | files: files,
+        status: determine_status(files)
+    }
+
+    {:reply, [], state}
+  end
+
+  defp determine_status(file_map) do
+    cond do
+      Enum.all?(
+        file_map,
+        fn {_f, status} -> status == :success end
+      ) ->
+        :sucess
+
+      Enum.any?(
+        file_map,
+        fn {_f, status} -> status == :error end
+      ) ->
+        :error
+
+      true ->
+        :pending
+    end
+  end
+end
